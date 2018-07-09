3f15ec00d9c28c114f23b9fd754ab0b0398dd51a
ZumDeWald <zumdewald@gmail.com>
2018-07-08 22:18:01 -0500

    Player moves
    Player can move up, down, left, right
    Player can still leave canvas

--- js/app.js
+++ js/app.js
@@ -12,12 +12,12 @@ const Enemy = function(y) {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function() {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + 2;
-    if (this.x === 510) {
+    this.x = this.x + this.dt;
+    if (this.x >= 510) {
       this.x = -100;
     };
 };
@@ -43,7 +43,23 @@ Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function() {
+Player.prototype.handleInput = function(e) {
+  switch (e){
+    case 'left': this.x = this.x - 100;
+    break;
+
+    case 'up': this.y = this.y - 100;
+    break;
+
+    case 'right': this.x = this.x + 100;
+    break;
+
+    case 'down': this.y = this.y + 100;
+    break;
+  }
+
+
+
 };
 
 
@@ -64,6 +80,12 @@ inky.y = 65;
 blinky.y = 145;
 pinky.y = 65;
 clyde.y = 225;
+
+//Set Enemy Speeds
+inky.dt = 2;
+blinky.dt = 1;
+pinky.dt = 0.5;
+clyde.dt = 3;
 
 
 
